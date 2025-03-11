74d7781bddbfb769af7acc0e369e9cd33bdec82f
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-03-11 17:18:12 +0200

    Задача 5

---
+++ 05. Trip/Program.cs
@@ -0,0 +1,27 @@
+﻿/**
+ * 05. Trip
+ */
+
+string destination = Console.ReadLine(); //дестинацията
+double targetBudget = double.Parse(Console.ReadLine()); //необходимите пари за пътуване
+double savedMoney = 0.0; //натрупалите се пари
+
+while (destination != "End")
+{
+    while (savedMoney <= targetBudget)
+    {
+        savedMoney += double.Parse(Console.ReadLine()); 
+    }
+
+    Console.WriteLine($"Going to {destination}!");
+    savedMoney = 0.0; //Занъляване на спестените пари за следващата дестинация
+
+    destination = Console.ReadLine();
+
+    if (destination == "End")
+    {
+        return;
+    }
+
+    targetBudget = double.Parse(Console.ReadLine());
+}
